7d3904f170f0566975dc335ea600793705371652
jose-quezada <jose.quezada29@inacapmail.cl>
2022-02-12 10:36:05 -0300

    ejemplo CreateDelete Archivos

---
+++ M6/clase4/index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const url = require("url");
+const port = 8080;
+http.createServer((req, res)=>{
+    if(req.url.includes("/usuarios")){
+        //tomar el nombre del usuario
+        const params = url.parse(req.url, true).query;
+        if(params.nombre=="Jose" && params.clave=="123456"){
+            res.write(`Bienvenido usuario ${params.nombre}`);
+        }
+        else{
+            res.write("Usuario o clave no validos");
+        }
+        
+    }
+    res.end();
+})
+.listen(port, ()=>{
+    console.log("Servidor iniciado")
+})
---
+++ M6/clase4/mi_repertorio/index.js
@@ -0,0 +1,63 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs")
+const port = 8080;
+
+http.createServer((req, res)=>{
+    console.log(req.url);
+    const params = url.parse(req.url, true).query;
+    console.log(params);//Object.nombre object.contenido 
+    if(req.url.includes("/crear")){
+        try{
+            fs.writeFile(
+                `archivos/${params.nombre}`,
+                params.contenido,
+                "utf-8",
+                (err)=>{
+                    //NO llamar a res aqui... pendiente de explicacion tecnica
+                    console.log(err);
+                    if(err){
+                        throw "se murio";
+                    }
+                    else{
+                        console.log(err);                        
+                    }
+                    
+                }
+            );
+            res.write(`Archivo ${params.nombre} creado con exito`);
+        }
+        catch(ex){
+            res.write("Ups!! hay un error");
+        }
+        
+    }
+    if(req.url.includes("/eliminar")){
+        try{
+            fs.unlink(
+                `archivos/${params.nombre}`,
+                function (err){
+                    if(err){
+                        console.log(err);
+                        throw "se murio";
+                    }
+                }
+            );
+            res.write(`Archivo ${params.nombre} Eliminado`)
+        }
+        catch(ex){
+            res.write("Ups!! hay un error");
+        }
+    }
+    if(req.url.includes("/leer")){
+
+    }
+    if(req.url.includes("/renombrar")){
+
+    }
+
+    res.end();
+})
+.listen(port, ()=>{
+    console.log(`Servidor Listo y escuchando el puerto ${port}`);
+})
