ab2f3716e380c31f09551c2489c2d8e02edab896
Jose Quezada <jose.quezadar@outlook.com>
2022-03-08 23:05:19 -0300

    Consolidado API

--- M7/Clase7/EjemploGym/consultas.js
+++ M7/Clase7/EjemploGym/consultas.js
@@ -1,33 +1,74 @@
 const { Pool } = require("pg");
-
-
-const config={  
+const pool = new Pool({
     user: "postgres",
     host: "localhost",
     password: "123456",
     port: 5432,
-    // Paso 1  
-    database: "gym"
-};
-const pool = new Pool(config);
+    // Paso 1
+    database: "gym",
+});
 // Paso 2
-const insertar = async (datos) => 
-{
+const insertar = async (datos) => {
     // Paso 3
-    const consulta = 
-    {    
+    const consulta = {
         text: "INSERT INTO ejercicios values($1, $2, $3, $4) RETURNING *",
         values: datos,
     };
     try {
         const result = await pool.query(consulta);
-        return {rows:result.rows, rowCount: result.rowCount};
-    } 
-    catch (error) {
+        return result;
+    } catch (error) {
         // Paso 4
         console.log(error.code);
-        return error;  
+        return error;
+    }
+}
+const consultar = async () => {
+    // Paso 2
+    try {
+        const result = await pool.query("SELECT * FROM ejercicios");
+        return result;
+    } catch (error) {
+        // Paso 3
+        console.log(error.code);
+        return error;
     }
 };
-// Paso 5
-module.exports = { insertar };
+// Paso 4
+const editar = async (datos) => {
+    // Paso 2
+    const consulta = {
+        text: `UPDATE ejercicios SET
+        nombre = $1,
+        series = $2,
+        repeticiones = $3,
+        descanso = $4
+        WHERE nombre = $1 RETURNING *`,
+        values: datos,
+    };
+    // Paso 3
+    try {
+        const result = await pool.query(consulta);
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+const eliminar = async (nombre) => {
+    // Paso 2
+    try {
+        const result = await pool.query(
+            `DELETE FROM ejercicios WHERE nombre = '${nombre}'  RETURNING *`
+        );
+        return result;
+    } catch (error) {
+        console.log(error.code);
+        return error;
+    }
+}
+
+// Paso 4
+module.exports = { insertar, consultar, editar, eliminar };
+
--- M7/Clase7/EjemploGym/index.js
+++ M7/Clase7/EjemploGym/index.js
@@ -1,30 +1,54 @@
 const http = require("http");
-// Paso 1
-const {insertar}= require("./consultas");
-// Paso 2
+const url = require("url");
+const { insertar, consultar, editar, eliminar } = require("./consultas");
 const fs = require("fs");
 
-http.createServer(async (req, res)=>{
-    if(req.url="/" && req.method=="GET"){
+http.createServer(async (req, res) => {
+    if (req.url == "/" && req.method === "GET") {
         res.setHeader("content-type", "text/html");
         const html = fs.readFileSync("index.html", "utf8");
         res.end(html);
     }
-    if(req.url == "/ejercicios" && req.method=="POST"){
-        console.log("Ejercicios");
-        let body ="";
-        req.on("data", chunk=>{
-            body +=chunk;
+
+    if ((req.url == "/ejercicios" && req.method == "POST")) {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk;
         });
-        req.on("end", async ()=>{
+        req.on("end", async () => {
+            // Paso 2
             const datos = Object.values(JSON.parse(body));
-            console.log("Datos:",datos);
+            // Paso 3
             const respuesta = await insertar(datos);
+            // Paso 4
             res.end(JSON.stringify(respuesta));
         });
     }
-})
-.listen(3000, ()=>{
+    if (req.url == "/ejercicios" && req.method === "GET") {
+        // Paso 2
+        const registros = await consultar();
+        // Paso 3
+        res.end(JSON.stringify(registros));
+    }
+    if (req.url == "/ejercicios" && req.method == "PUT") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+        req.on("end", async () => {
+            const datos = Object.values(JSON.parse(body));
+            // Paso 2
+            const respuesta = await editar(datos);
+            res.end(JSON.stringify(respuesta));
+        });
+    }
+    if (req.url.startsWith("/ejercicios?") && req.method == "DELETE") {
+        // Paso 3
+        const { nombre } = url.parse(req.url, true).query;
+        // Paso 4
+        const respuesta = await eliminar(nombre);
+        res.end(JSON.stringify(respuesta));
+    }
+}).listen(3000, () => {
     console.log("Ejecutando");
 });
-           
